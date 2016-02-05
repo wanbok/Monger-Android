6c4c3b088e9608ee8195ab10542a3893f0b51c9e
Wanbok Choi <choi@wanbok.com>
2016-02-06 04:24:25 +0900

    Monger can be run as a standalone service.

--- app/src/main/java/com/wanbok/smscatcher/MainActivity.kt
+++ app/src/main/java/com/wanbok/smscatcher/MainActivity.kt
@@ -1,38 +1,51 @@
 package com.wanbok.monger
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sharedPref = baseContext?.getSharedPreferences("com.wanbok.monger.MONGER", Context.MODE_PRIVATE)
-
-        val url = sharedPref?.getString("URL", "")
-        val channel = sharedPref?.getString("CHANNEL", "")
-
-        val urlText = findViewById(R.id.url) as? EditText
-        val channelText = findViewById(R.id.channel) as? EditText
-
-        urlText?.setText(url)
-        channelText?.setText(channel)
+        onLoad(null)
     }
 
-    fun onSave(button: View) {
-        val editor = baseContext?.getSharedPreferences("com.wanbok.monger.MONGER", Context.MODE_PRIVATE)?.edit()
+    fun openSlackSite(button: View?) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(Uri.parse("https://my.slack.com/services/new/incoming-webhook/"))
+        startActivity(intent)
+    }
 
-        val channelText = findViewById(R.id.channel) as? EditText
+    fun onLoad(button: View?) {
+        val sharedPref = getSharedPreferences("com.wanbok.monger.MONGER", Context.MODE_PRIVATE)
+
+        val url = sharedPref?.getString("URL", "")
         val urlText = findViewById(R.id.url) as? EditText
+        urlText?.setText(url)
+        Log.i(TAG, "Load, URL : " + url)
+    }
 
-        editor?.putString("URL", urlText?.text?.toString() ?: "")
-        editor?.putString("CHANNEL", channelText?.text?.toString() ?: "")
+    fun onSave(button: View?) {
+        val editor = getSharedPreferences("com.wanbok.monger.MONGER", Context.MODE_PRIVATE)?.edit()
+
+        val urlText = findViewById(R.id.url) as? EditText
+        val url = urlText?.text?.toString() ?: ""
+        editor?.putString("URL", url)
+        editor?.commit()
+
+        Log.i(TAG, "Save, URL : " + url)
+        Toast.makeText(this, "Saved Incoming WebHook!", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/wanbok/smscatcher/SMSReceiver.kt
+++ app/src/main/java/com/wanbok/smscatcher/SMSReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.telephony.SmsMessage
 import android.util.Log
 import com.github.kittinunf.fuel.Fuel
+import com.github.salomonbrys.kotson.jsonArray
 import com.github.salomonbrys.kotson.jsonObject
 
 /**
@@ -25,27 +26,51 @@ class SMSReceiver: BroadcastReceiver() {
                 SmsMessage.createFromPdu(it as? ByteArray, bundle?.getString("format"))
             }
 
-            onSmsReceived(messages)
+            onSmsReceived(context, messages)
         }
     }
 
-    private fun onSmsReceived(messages: List<SmsMessage>?) {
+    private fun onSmsReceived(context: Context?, messages: List<SmsMessage>?) {
         messages?.forEach {
             Log.i(TAG, it.messageBody)
             Log.i(TAG, it.originatingAddress)
+            val sharedPref = context?.getSharedPreferences("com.wanbok.monger.MONGER", Context.MODE_PRIVATE)
+            val url = sharedPref?.getString("URL", "")
+
+            val sender = if (it.displayOriginatingAddress == it.originatingAddress)
+                (it.displayOriginatingAddress ?: "")
+            else {
+                (it.displayOriginatingAddress ?: "") + " (" + (it.originatingAddress ?: "") + ")"
+            }
 
             val json = jsonObject(
                     "response_type" to "in_channel",
-                    "text" to (it.messageBody ?: "") + " :calling: _" + (it.originatingAddress ?: "")+"_"
+                    "username" to "monger-bot",
+                    "icon_emoji" to ":email:",
+                    "text" to (it.messageBody ?: ""),
+                    "attachments" to jsonArray(
+                            jsonObject(
+                                    "color" to "#36a64f",
+                                    "fields" to jsonArray(
+                                            jsonObject(
+                                                    "title" to sender,
+                                                    "short" to true
+                                            )
+                                    )
+                            )
+                    )
             )
-            Fuel.post("https://hooks.slack.com/services/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").body(json.toString())
-                .response { request, response, result ->
-                    result.fold({ d ->
-                        //do something with data
-                    }, { err ->
-                        //do something with error
-                    })
-                }
+
+            if (url != null) {
+                Fuel.post(url).body(json.toString())
+                        .response { request, response, result ->
+                            result.fold({ d ->
+                                //do something with data
+                            }, { err ->
+                                //do something with error
+                            })
+                        }
+            }
         }
     }
 }
